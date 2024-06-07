06c5b2a1c5ba5f3273b65f97c2655d007bf075a7
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-06-07 14:54:42 +0300

    main

---
+++ App601/Project/SratrScreens/Loading/LoadingView.swift
@@ -0,0 +1,30 @@
+//
+//  LoadingView.swift
+//  App601
+//
+//  Created by IGOR on 05/06/2024.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+
+        ZStack {
+            
+            Color("bg")
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Image("Llogo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+    }
+}
+
+#Preview {
+    LoadingView()
+}
